d32e58ee76d7889cd4b5b4dd01ed02fc44ce4a84
Alexander Pykavy <aleksandrpykavyj@gmail.com>
2025-08-05 08:59:45 +0200

    Make worker thread-safe

--- worker/worker.go
+++ worker/worker.go
@@ -6,29 +6,43 @@ import (
 	"maps"
 	"os"
 	"os/exec"
+	"sync"
 
 	"github.com/google/uuid"
 )
 
 type Worker struct {
+	sync.RWMutex
+
 	Tasks map[uuid.UUID]*task.Task
 }
 
 func (w *Worker) ListTasks() iter.Seq[*task.Task] {
+	w.RLock()
+	defer w.RUnlock()
+
 	return maps.Values(w.Tasks)
 }
 
 func (w *Worker) GetTask(id uuid.UUID) *task.Task {
+	w.RLock()
+	defer w.RUnlock()
+
 	return w.Tasks[id]
 }
 
 func (w *Worker) StartTask(t task.Task) error {
+	w.Lock()
+
 	if _, ok := w.Tasks[t.ID]; ok {
+		w.Unlock()
 		return task.ErrAlreadyExists
 	}
 
 	t.Cmd = exec.Command(t.Executable, t.Args...)
 	w.Tasks[t.ID] = &t
+
+	w.Unlock()
 
 	stdout, err := t.Cmd.CombinedOutput()
 	os.Stdout.Write(stdout)
@@ -40,7 +54,10 @@ func (w *Worker) StartTask(t task.Task) error {
 }
 
 func (w *Worker) StopTask(id uuid.UUID) error {
-	t := w.GetTask(id)
+	w.Lock()
+	defer w.Unlock()
+
+	t := w.Tasks[id]
 	if t == nil {
 		return task.ErrNotExists
 	}
